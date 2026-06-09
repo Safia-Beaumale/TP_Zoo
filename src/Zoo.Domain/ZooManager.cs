26a0a81cb1fc9ff34dbcdfc22a031861e6184d66
Safia Beaumale-Mesmar <safia.beaumale-mesmar@epitech.eu>
2026-06-09 16:51:49 +0200

    [ADD] Init & Squelette de départ

---
+++ src/Zoo.Domain/ZooManager.cs
@@ -0,0 +1,15 @@
+namespace Zoo.Domain;
+
+public class ZooManager
+{
+    public const int MaxCapacity = 50;
+
+    public int AddAnimal(Animal animal) => throw new NotImplementedException();
+    public Animal? GetAnimal(int id) => throw new NotImplementedException();
+    public int TotalAnimals => throw new NotImplementedException();
+    public int TotalCapacityUsed => throw new NotImplementedException();
+    public double CalculateDailyRation(int animalId) => throw new NotImplementedException();
+    public double CalculateDailyCost() => throw new NotImplementedException();
+    public IReadOnlyList<Animal> GetCriticalAnimals() => throw new NotImplementedException();
+    public bool RemoveAnimal(int id) => throw new NotImplementedException();
+}
